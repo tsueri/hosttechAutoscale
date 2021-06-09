4297805ac1d66e17713445e47b877d94d0b9ee2c
Jonas Zürcher <jonas.zuercher@spschweiz.ch>
2021-06-09 23:28:00 +0200

    check if jq is installed and offer to install

--- getServerUUID.sh
+++ getServerUUID.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+if ! command -v jsq >/dev/null; then
+  read -p "This script requires jq to be installed and on your PATH. Would you like to install jq now? (Y/N)" answer
+    if [[ "$answer" == "y" || "$answer" == "Y"  ]]; then
+      sudo apt update && sudo apt install -y jq
+    else
+      echo "jq is reqiered. Exit now."
+      exit 1
+    fi
+fi
+
 echo "Enter your User ID and your API Token and get a list of available Servers on your hosttech.cloud"
 read -p "Please enter your USER_UUID as found on https://hosttech.cloud/Easy/APIs/: " USER_UUID
 read -p "Please enter your API-Token as found on https://hosttech.cloud/Easy/APIs/: " API_TOKEN
-
-USER_UUID=96761224-d36c-4615-86b1-73dff67301b7
-API_TOKEN=8c21bbeecbf4a8d3073ca088cda1a743a6bc22493ff5905252458e0151203caf
 
 serversjson=$(curl -H "Content-Type: application/json" \
 -H "X-Auth-UserId: $USER_UUID" \
